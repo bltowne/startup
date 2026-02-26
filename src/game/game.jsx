320c7858e493e4d9d61a18f250ca984bd11d2357
Bree <bltowne@outlook.com>
2026-02-26 15:51:31 -0700

    react 2 game countdown

--- src/game/game.jsx
+++ src/game/game.jsx
@@ -1,12 +1,27 @@
 import React from 'react';
 import "../app.css";
+import { useNavigate } from "react-router-dom";
 
 export function Game() {
+  const navigate = useNavigate();
+  const [time, setTimer] = React.useState(30);
+  const [remainingTime, setRemainingTime] = React.useState(30);
+
+  React.useEffect(() => {
+    if (remainingTime === 0) {
+      navigate('/scoreboard');
+    }
+    const timer = setTimeout(() => {
+      setRemainingTime(remainingTime - 1);
+    }, 1000);
+    return () => clearTimeout(timer);
+  }, [remainingTime, navigate]);
+
   return (
     <main>
-        <h2>You have 30 seconds to answer!</h2>
+        <h2>You have {time} seconds to answer!</h2>
         <br />
-        <h1>24</h1>
+        <h1>{remainingTime}</h1>
         <br />
         <div className="answer-container">
             <h3>Question? (Database Placeholder)</h3>
--- src/scoreboard/scoreboard.jsx
+++ src/scoreboard/scoreboard.jsx
@@ -1,48 +1,51 @@
 import React from 'react';
 import "../app.css";
+import { useNavigate } from "react-router-dom";
 
 export function Scoreboard( { user } ) {
-  return (
-    <main>
-        <h1>SCOREBOARD (Websocket Placeholder)</h1>
-        <div className="score-container">
-            <div className="gray-scoreboard">
-                <h2>{user}</h2><h4>150</h4>
+    const navigate = useNavigate();
+
+    return (
+        <main>
+            <h1>SCOREBOARD</h1>
+            <div className="score-container">
+                <div className="gray-scoreboard">
+                    <h2>{user}</h2><h4>150</h4>
+                </div>
+                <div className="gray-scoreboard">
+                    <h2>Team B</h2><h4>120</h4>
+                </div>
             </div>
-            <div className="gray-scoreboard">
-                <h2>Team B</h2><h4>120</h4>
-            </div>
-        </div>
-        <br />
-        <h3>Question: (Database Placeholder)</h3>
-        <table>
-            <tr>
-                <td className="answer">Answer 1</td>
-                <td className="points">50</td>
-                <td className="answer">Answer 5</td>
-                <td className="points">10</td>
-            </tr>
-            <tr>
-                <td className="answer">Answer 2</td>
-                <td className="points">30</td>
-                <td className="answer">Answer 6</td>
-                <td className="points">5</td>
-            </tr>
-            <tr>
-                <td className="answer">Answer 3</td>
-                <td className="points">20</td>
-                <td className="answer">Answer 7</td>
-                <td className="points">3</td>
-            </tr>
-            <tr>
-                <td className="answer">Answer 4</td>
-                <td className="points">15</td>
-                <td className="answer">Answer 8</td>
-                <td className="points">1</td>
-            </tr>
-        </table>
-        <br />
-        <input type="button" id="button" value="Ready for Next Round" />
-    </main>
-  );
+            <br />
+            <h3>Question: </h3>
+            <table>
+                <tr>
+                    <td className="answer">Answer 1</td>
+                    <td className="points">50</td>
+                    <td className="answer">Answer 5</td>
+                    <td className="points">10</td>
+                </tr>
+                <tr>
+                    <td className="answer">Answer 2</td>
+                    <td className="points">30</td>
+                    <td className="answer">Answer 6</td>
+                    <td className="points">5</td>
+                </tr>
+                <tr>
+                    <td className="answer">Answer 3</td>
+                    <td className="points">20</td>
+                    <td className="answer">Answer 7</td>
+                    <td className="points">3</td>
+                </tr>
+                <tr>
+                    <td className="answer">Answer 4</td>
+                    <td className="points">15</td>
+                    <td className="answer">Answer 8</td>
+                    <td className="points">1</td>
+                </tr>
+            </table>
+            <br />
+            <input type="button" id="button" value="Ready for Next Round" onClick={() => navigate('/waiting')} />
+        </main>
+    );
 }
